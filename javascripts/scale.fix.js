4222e990ee257fae1abefcf6db95382425741878
Daniela Villacreces <140455247+danielavivi@users.noreply.github.com>
2024-11-17 15:45:40 -0500

    Update scale.fix.js

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,19 +1,9 @@
 var metas = document.getElementsByTagName('meta');
 var i;
 var coll = document.getElementsByClassName("collapsible");
-var a;
+var j;
 
-for (a = 0; a < coll.length; a++) {
-  coll[a].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+
 if (navigator.userAgent.match(/iPhone/i)) {
   for (i=0; i<metas.length; i++) {
     if (metas[i].name == "viewport") {
@@ -28,4 +18,15 @@ function gestureStart() {
       metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
     }
   }
+}
+for (j = 0; j < coll.length; j++) {
+  coll[j].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
 }
