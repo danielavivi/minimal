15e01c00828cee9e19b73700aa1f252d51b30221
Daniela Villacreces <140455247+danielavivi@users.noreply.github.com>
2024-11-17 15:33:54 -0500

    Update scale.fix.js

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,5 +1,19 @@
 var metas = document.getElementsByTagName('meta');
 var i;
+var coll = document.getElementsByClassName("collapsible");
+var a;
+
+for (a = 0; a < coll.length; a++) {
+  coll[a].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
 if (navigator.userAgent.match(/iPhone/i)) {
   for (i=0; i<metas.length; i++) {
     if (metas[i].name == "viewport") {
